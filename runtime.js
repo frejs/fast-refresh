0ed499f017e3bad251ea928177f2e9f0f44defa0
changhaozhao <changhaozhao@trip.com>
2021-07-29 11:58:37 +0800

    finished

--- runtime.js
+++ runtime.js
@@ -1,3 +1,20 @@
-function register(type,id){
-    
+import { getCurrentFiber, schedule } from 'fre'
+
+let graph = new Map()
+let queue = new Set()
+
+export function register(type, id) {
+    graph.set(type, id) // fn => name
+}
+
+export function sign() {
+    return (type, key) => { // fn => key
+        const current = getCurrentFiber()
+        queue.push(current)
+        
+    }
+}
+
+export function refresh() {
+    queue.some(c => schedule(c))
 }
